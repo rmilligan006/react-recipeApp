e1a2bd5ea003271ee558518a16203f17e50a6a9e
Ryan Milligan <90968645+rmilligan006@users.noreply.github.com>
2022-06-29 14:32:00 -0400

    small changes

--- src/components/Meal.jsx
+++ src/components/Meal.jsx
@@ -32,12 +32,6 @@ const Meal = () => {
       <div className="main">
         <div className="heading">
           <h1>Search For your next meal!</h1>
-          <h4>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
-            mollitia hic optio, delectus in soluta inventore temporibus iusto
-            qui quia obcaecati quae rem atque quas. Perferendis esse natus id
-            laudantium?
-          </h4>
         </div>
         <div className="searchBox">
           <input
